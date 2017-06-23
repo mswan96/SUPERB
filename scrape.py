06e4e5366b2386b78af82108a8514842af5f4918
Megan Swanson <mswan96@uw.edu>
2017-06-22 18:59:51 -0700

    updated scrape.py

--- scrape.py
+++ scrape.py
@@ -111,6 +111,87 @@ def processDay( year, month, day ):
 return avgRenewables, avgAll
 
 
+def processMonth( year, month ):
+	totalRenew = np.empty(7)
+	totalAll = np.empty(5)
+
+	avgRenew = np.empty(7)
+	avgAll = np.empty(5)
+
+	if month == 4: # Finish April
+		days = 10
+		for d in xrange(20, 31):
+			Renew, All = processDay(year, month, d)
+			for i in xrange(0, 7):
+				totalRenew[i] += Renew[i]
+			for j in xrange(0, 5):
+				totalAll[j] += All[j]
+			
+	else if month == 2:  # february has 28 days
+		days = 28
+		for d in xrange(1, 29)
+			Renew, All = processDay(year, month, d)
+			for i in xrange(0, 7):
+				totalRenew[i] += Renew[i]
+			for j in xrange(0, 5):
+				totalAll[j] += All[j]
+
+	else if month == (6 or 9 or 11): # Months with 30 days
+		days = 30
+		for d in xrange(1, 31)
+			Renew, All = processDay(year, month, d)
+			for i in xrange(0, 7):
+				totalRenew[i] += Renew[i]
+			for j in xrange(0, 5):
+				totalAll[j] += All[j]	
+	else if :  # Months with 31 days
+		days = 31
+		for d in xrange(1, 32)
+			Renew, All = processDay(year, month, d)
+			for i in xrange(0, 7):
+				totalRenew[i] += Renew[i]
+			for j in xrange(0, 5):
+				totalAll[j] += All[j]
+
+	for k in xrange(0, 7):
+		avgRenew[k] = totalRenew[k]/days
+	for l in xrange(0, 5):
+		avgAll[l] = totalAll[l]/days
+
+	return avgRenew, avgAll
+
+
+def processYear( year ):
+	totalRenew = np.empty(7)
+	totalAll = np.empty(5)
+
+	avgRenew = np.empty(7)
+	avgAll = np.empty(5)
+
+	if year == 2010: 
+		months = 9
+		for m in xrange(4, 13): # Finish April
+			Renew, All = processMonth(year, m)
+			for i in xrange(0, 7):
+				totalRenew[i] += Renew[i]
+			for j in xrange(0, 5):
+				totalAll[j] += All[j]
+	else:
+		months = 12
+		for m in xrange(1, 13): # Finish April
+			Renew, All = processMonth(year, m)
+			for i in xrange(0, 7):
+				totalRenew[i] += Renew[i]
+			for j in xrange(0, 5):
+				totalAll[j] += All[j]
+	
+	for k in xrange(0, 7):
+		avgRenew[k] = totalRenew[k]/days
+	for l in xrange(0, 5):
+		avgAll[l] = totalAll[l]/days
+
+	return avgRenew, avgAll	
+
 def main(argv):
 	if len(sys.argv) < 2: # No input arguments, aggregate all data since 4/20/2010
 		year = None
@@ -131,7 +212,9 @@ def main(argv):
 	else:
 		sys.exit("Error: cannot input more than three arguments")
 
-
+	avgDayRenew = np.empty(7)
+	avgDayAll = np.empty(5)
+	dayCount = 0
 
 	i = datetime.datetime.now()
 
@@ -151,22 +234,53 @@ def main(argv):
 
 	if year == None:  # Aggregate all data
 		# Start from 04/20/2010
-		for aprilDays in xrange(20, 31): # Finish April
-			processDay(2010, 4, aprilDays)
+		months = 12
+		for y in xrange(2010, 13):
+			Renew, All = processYear(year)
+			for i in xrange(0, 7):
+				totalRenew[i] += Renew[i]
+			for j in xrange(0, 5):
+				totalAll[j] += All[j]
+	else:
+		Renew, All = processYear(year)
+			for i in xrange(0, 7):
+				totalRenew[i] += Renew[i]
+			for j in xrange(0, 5):
+				totalAll[j] += All[j]
 
-		for firstM in xrange(5, 13): # Finish 2010
-			if firstM == (6 or 9 or 11): # Months with 30 days
-				for firstD in xrange(1, 31)
-					processDay(2010, firstM, firstD)
-			else:  # Months with 31 days (february not included in 2010)
-				for firstD in xrange(1, 32)
-					processDay(2010, firstM, firstD)
-		# Now the rest up until yesterday
-		for y in xrange(2011, currYear):
-			for m in xrange(1, 13)
-				if m == 2:
+		# for aprilDays in xrange(20, 31): # Finish April
+		# 	Renew1, All1 = processDay(2010, 4, aprilDays)
+		# 	dayCount += 1
+		# 	for j in xrange(0, 7):
+		# 		avgDayRenew[j] += Renew1[j]
+		# 		avgDayAll[j] += All1[j]
 
-			processDay(year, month, day)
+		# for firstM in xrange(5, 13): # Finish 2010
+		# 	if firstM == (6 or 9 or 11): # Months with 30 days
+		# 		for firstD in xrange(1, 31)
+		# 			Renew2, All2 = processDay(2010, firstM, firstD)
+		# 			dayCount += 1
+		# 			for j in xrange(0, 7):
+		# 				avgDayRenew[j] += Renew2[j]
+		# 				avgDayAll[j] += All2[j]
+		# 	else:  # Months with 31 days (february not included in 2010)
+		# 		for firstD in xrange(1, 32)
+		# 			Renew2, All2 = processDay(2010, firstM, firstD)
+		# 			dayCount += 1
+		# 			for j in xrange(0, 7):
+		# 				avgDayRenew[j] += Renew2[j]
+		# 				avgDayAll[j] += All2[j]
+		# # Now the rest up until yesterday
+		# for y in xrange(2011, currYear):
+		# 	for m in xrange(1, 13)
+		# 		if m == 2:  # february has 28 days
+		# 			for firstD in xrange(1, 32)
+		# 			Renew2, All2 = processDay(2010, firstM, firstD)
+		# 			dayCount += 1
+		# 			for j in xrange(0, 7):
+		# 				avgDayRenew[j] += Renew2[j]
+		# 				avgDayAll[j] += All2[j]
+		# 	processDay(year, month, day)
 	 
 
 
