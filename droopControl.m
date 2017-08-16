dc8bd839d0c489dbafe16d9d793497a9fde4bb6b
mswan96 <mswan96@uw.edu>
2017-08-15 20:33:12 -0700

    Update Comments

--- droopControl.m
+++ droopControl.m
@@ -1,5 +1,14 @@
+% droopControl.m
+% Megan Swanson
+% SUPERB Project 2017
+%
+%       Swing equation with Droop Control variable. Takes coefficients A and B 
+%       and droop coefficient R as inputs and solves with respect to the step 
+%       disturbance p.
+
 function dfdt = droopControl(t, f, A, B, R)
 
+% Step disturbance
 if (t >= 0.05) && (t <= 0.1)
     p = -1;
 else
