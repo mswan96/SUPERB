c8b4c4965c9490c1e7fbbc9a51341aa02d16fabe
mswan96 <mswan96@uw.edu>
2017-08-15 20:35:17 -0700

    Update comments

--- virtualInertia.m
+++ virtualInertia.m
@@ -1,5 +1,14 @@
+% virtualInertia.m
+% Megan Swanson
+% SUPERB Project 2017
+%
+%       Swing equation with Virtual Inertia control variable. Takes 
+%       coefficients A and B and virtual inertia value M as inputs 
+%       and solves with respect to the step disturbance p.
+
 function dfdt = virtualInertia(t, f, A, B, M)
 
+% Step disturbance
 if (t >= 0.05) && (t <= 0.1)
     p = -1;
 else
